586aaf9369b40785f67b654a9441814bf35c327a
root <root@lamp.(none)>
2014-10-13 23:31:41 +0000

    Added Readme

--- composer/wpPrimer.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#download/update wordpress
-php /var/www/composer/composer.phar install;
-php /var/www/composer/composer.phar update;
-wait
-echo "Moving wordpress folder and wp-config.php"
-mv ./vendor/wordpress ../
-mv ./wp-config.php ../wordpress
--- wordpress/github-post-receive.php
+++
@@ -1,8 +0,0 @@
-<?php
-ignore_user_abort(true);
-
-if (!empty($_POST['payload'])) {
-$file = fopen( '/root/git-pull-requests/example', 'w+' );
-fclose( $file );
-}
-?>
